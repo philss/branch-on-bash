ea85b694aba68fd65e55209767d9097664e68d82
Philip Sampaio <philip.sampaio@gmail.com>
2011-06-03 21:11:57 -0300

    Fix bug - working with ruby 1.9.2 now

--- git_branch.rb
+++ git_branch.rb
@@ -1,7 +1,7 @@
 green  = "\033[0;32m"
 normal = "\033[0m"
 red    = "\033[0;31m"
-current_branch = `git branch 2>/dev/null`.grep(/^\*/).first
+current_branch = `git branch 2>/dev/null`.split(%r{\n}).grep(/^\*/).first
 if current_branch
 	branch_name = current_branch.gsub(/^\*\s*/,'').strip
 	color = branch_name  =~ /master/ ? green : red
